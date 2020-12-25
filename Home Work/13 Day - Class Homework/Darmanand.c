4018b8882edb0a03be24c58dd1cab437cf1ce363
Danny <khorasanizadeh.danial@gmail.com>
2020-12-25 12:30:29 +0330

    13 Day Class Homework

---
+++ Home Work/13 Day - Class Homework/Darmanand.c
@@ -0,0 +1,38 @@
+/*
+--------------------------------------------------
+Name    : Darmanand
+Author  : Danial Khorasanizadeh
+Course  : Foundations of programming (Fall 99)
+Date    : 23/Dec/2020
+--------------------------------------------------
+*/
+#include <stdio.h>
+void darmanand(char name1[21], char name2[21])
+{
+    printf("%s to %s: ke ba in dar agar dar bande dar manand, dar manand.\n", name1, name2);
+    printf("%s to %s: dar manand?\n", name2, name1);
+    printf("%s to %s: kdar manand.\n", name1, name2);
+}
+int main(void)
+{
+    int n;
+    char names[100][21] = {0};
+    scanf("%d ", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%s", names[i]);
+    }
+    for (int i = 0; i + 1 < n; i++)
+    {
+        printf("%s to %s: ke ba in dar agar dar bande dar manand, dar manand.\n", names[i], names[i + 1]);
+        for (int j = i + 1; j > 0; j--)
+        {
+            printf("%s to %s: dar manand?\n", names[j], names[j - 1]);
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%s to %s: dar manand.\n", names[j], names[j + 1]);
+        }
+    }
+    return 0;
+}
